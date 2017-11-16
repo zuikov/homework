71b652e2fc817eeba02439f813ae0803ba72c733
zuikov <33103050+zuikov@users.noreply.github.com>
2017-11-16 15:15:31 +0200

    It is a test to add a file in repository

---
+++ homework3/index.js
@@ -0,0 +1,11 @@
+//---------------------------------------------------Default state 
+
+
+
+//-------------------Экспорт результатов
+module.exports = {
+    firstName: 'andrei',
+    secondName: 'zuikov',
+    task: ProjectModule   
+}
+
